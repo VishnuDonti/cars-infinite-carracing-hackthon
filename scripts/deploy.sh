f3b08ea395deaa64b5183d94a59a637d98b89b62
SAUMA <vishnuvardhan.donti@gmail.com>
2021-02-12 20:04:28 +0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "12333"
-ssh -o "StrictHostKeyChecking no" -i scripts/privatekey root:abc@2.50.102.0 -p 9090
+ssh -o "StrictHostKeyChecking no" -i /scripts/privatekey root:abc@2.50.102.0 -p 9090
 abc
 mkdir abc
