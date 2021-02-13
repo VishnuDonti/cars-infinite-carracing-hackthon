5d83570f46d9edf844df7546633f694dd942df56
SAUMA <vishnuvardhan.donti@gmail.com>
2021-02-13 09:27:43 +0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 chmod 600 $TRAVIS_BUILD_DIR/scripts/privately
 value=$(<$TRAVIS_BUILD_DIR/scripts/python.py)
-ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/scripts/privately root@5.107.89.225 -p 9090 <<EOF
+ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/scripts/privately root@$FOO -p 9090 <<EOF
 cd /var
 touch python.py
 echo "$value" > python.py
