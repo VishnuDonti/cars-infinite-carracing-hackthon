d7fa644109ab2b15adee943e1ca0b01f647322d2
SAUMA <vishnuvardhan.donti@gmail.com>
2021-02-13 07:48:30 +0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,6 @@ echo "12333"
 chmod 600 $TRAVIS_BUILD_DIR/scripts/privately
 ssh -o "StrictHostKeyChecking no" -i $TRAVIS_BUILD_DIR/scripts/privately root:abc@2.50.102.0 -p 9090 <<EOF
 mkdir abc
+cp $TRAVIS_BUILD_DIR/python.py python.py
+pwd
 EOF
